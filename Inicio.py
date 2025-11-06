7da7982e839016adce25eab1a33f34eb6be62b37
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2025-11-06 10:14:26 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -38,8 +38,8 @@ def send_mqtt_message(topic, message):
 st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_sidebar_state="collapsed")
 # Streamlit page setup
 st.title("Análisis de Imagen:🤖🏞️")
-image = Image.open('OIG4.jpg')
-st.image(image, width=350)
+#image = Image.open('OIG4.jpg')
+#st.image(image, width=350)
 
 with st.sidebar:
     st.subheader("Este Agente analiza el contenido de la imagen y responde tus preguntas.")
